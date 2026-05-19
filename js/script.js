dcd7f4bb1981fcea87f673eb222e89c6f38c3906
marimche <test@gmail.com>
2026-05-19 05:35:55 +0300

    base layout for test landing

---
+++ js/script.js
@@ -0,0 +1,128 @@
+// -------------- Слайдер для участников турнира ------------------------------ 
+const participantsSlides = document.querySelector('.participants-list__slides'); //track
+
+const participantSlideCount = document.getElementById("participants-list").children?.length;
+
+const prevButtonParticipants = document.getElementById("prev");
+const nextButtonParticipants = document.getElementById("next");
+
+const slider = document.querySelector('.participants-list');
+
+let autoPlayInterval;
+let currentIndexP = 0;
+
+const firstClone = participantsSlides.children[0].cloneNode(true);
+const lastClone = participantsSlides.children[participantsSlides.children.length - 1].cloneNode(true);
+
+participantsSlides.appendChild(firstClone);
+participantsSlides.insertBefore(lastClone, participantsSlides[0]);
+
+const allSlides = Array.from(participantsSlides.children);
+const totalSlides = allSlides.length;
+
+const updatePosition = (animate = true) => {
+  if (!animate) {
+  participantsSlides.style.transition = 'none';
+  } else {
+  participantsSlides.style.transition = 'transform 0.3s ease';
+  }
+
+  const totalWidth = 394; // 2364
+  
+  const offset = -currentIndexP * totalWidth;
+  participantsSlides.style.transform = `translateX(${offset}px)`;
+};
+    
+currentIndexP = 1;
+updatePosition(false);
+
+const next = () => {
+  if (currentIndexP>= totalSlides - 1) {
+    currentIndexP = 1;
+    updatePosition(false);
+    setTimeout(() => {
+      currentIndexP++;
+      updatePosition(true);
+    }, 20);
+  } else {
+    currentIndexP++;
+    updatePosition(true);
+  }
+};
+
+const prev = () => {
+  if (currentIndexP <= 0) {
+    currentIndexP = totalSlides - 2;
+    updatePosition(false);
+    setTimeout(() => {
+      currentIndexP--;
+      updatePosition(true);
+    }, 20);
+  } else {
+    currentIndexP--;
+    updatePosition(true);
+  }
+}
+
+prevButtonParticipants.addEventListener('click', () => {
+  next();
+});
+
+nextButtonParticipants.addEventListener('click', () => {
+  prev();
+});
+
+function startAutoPlay() {
+  autoPlayInterval = setInterval(() => {
+    next();
+  }, 4000);
+}
+
+function stopAutoPlay() {
+    clearInterval(autoPlayInterval);
+}
+
+startAutoPlay();
+
+slider.addEventListener('mouseenter', stopAutoPlay);
+slider.addEventListener('mouseleave', startAutoPlay);
+
+
+
+// -------------- Слайдер для этапов преображения Васюков ------------------------------
+const transformationSlides = document.querySelector('.transformation-list__slides');
+
+const transformationSlideCount = document.getElementById("transformation-list").children?.length;
+
+const prevButtonTransformation = document.getElementById("transformation__prev");
+const nextButtonTransformation = document.getElementById("transformation__next");
+
+function goToPoint(index) {
+  if (index < 0) {
+    index = transformationSlideCount - 1;
+  } else if (index >= transformationSlideCount) {
+    index = 0;
+  }
+
+  currentIndexT = index;
+  transformationSlides.style.transform = `translateX(${-index/transformationSlideCount * 100}%)`;
+}
+
+function markLabel(activIndex, inactivIndex) {
+  const activeLabel = document.getElementById("transformation-list__controls-points").children?.[activIndex];
+  const inactiveLabel = document.getElementById("transformation-list__controls-points").children?.[inactivIndex];
+  activeLabel.classList.add('active');
+  inactiveLabel.classList.remove('active');
+}
+
+prevButtonTransformation.addEventListener('click', () => {
+  markLabel(currentIndexT - 1, currentIndexT);
+  goToPoint(currentIndexT - 1);
+});
+
+nextButtonTransformation.addEventListener('click', () => {
+  markLabel(currentIndexT + 1, currentIndexT);
+  goToPoint(currentIndexT + 1);
+});
+
+goToPoint(0);
